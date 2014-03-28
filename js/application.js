9a4cbcc7fa079d6c6253bec9e7bc1c8d02c91b35
Randy Pond <randito@gmail.com>
2014-03-28 01:34:00 -0500

    http://emberjs.com/guides/getting-started/using-other-adapters/

--- js/application.js
+++ js/application.js
@@ -2,6 +2,8 @@
 (function() {
   window.Todos = Ember.Application.create();
 
-  Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
+  Todos.ApplicationAdapter = DS.LSAdapter.extend({
+    namespace: 'todos-emberjs'
+  });
 
 }).call(this);
